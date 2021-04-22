704ba5570c638225c6383e2b417bd97f15f34830
Smartex <nurgul.aisariyeva@smartex.kz>
2021-04-22 14:58:51 +0600

    video callcontent controllers modified

--- SMVideoCall/VideoCallViewController.swift
+++ SMVideoCall/VideoCallViewController.swift
@@ -20,6 +20,7 @@ public class VideoCallViewController: UIViewController, WKNavigationDelegate, WK
     public override func loadView() {
         let contentController = WKUserContentController()
         contentController.add(self, name: "endCallWV")
+        contentController.add(self, name: "snapScreen")
         
         let webconfig = WKWebViewConfiguration()
         webconfig.allowsInlineMediaPlayback = true
@@ -45,6 +46,31 @@ public class VideoCallViewController: UIViewController, WKNavigationDelegate, WK
             navigationController?.popViewController(animated: true)
             dismiss(animated: true, completion: nil)
         }
+        
+        if message.name == "snapScreen" {
+            takeScreenShot()
+        }
+    }
+    
+    func takeScreenShot(){
+        let bounds = UIScreen.main.bounds
+        UIGraphicsBeginImageContextWithOptions(bounds.size, true, 0.0)
+        self.view.drawHierarchy(in: bounds, afterScreenUpdates: true)
+        let screenshotImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        let base64 = convertImageToBase64String(img: screenshotImage!)
+        let script = "window.onSnapScreen(\"\(base64)\")"
+        webView.evaluateJavaScript(script) { (result, error) in
+            if let result = result {
+                print("Result: \(result)")
+            } else if let error = error {
+                print("An error occurred: \(error)")
+            }
+        }
+    }
+    
+    func convertImageToBase64String (img: UIImage) -> String {
+        return img.jpegData(compressionQuality: 1)?.base64EncodedString() ?? ""
     }
 }
 
